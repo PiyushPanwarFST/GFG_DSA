943b1b9dd2f4dfee13214d284224a2f74f089d2a
PiyushPanwarFST <piyush2002panwar@gmail.com>
2025-09-21 19:46:54 +0530

    Sorting

---
+++ 04_Sorting/practise.py
@@ -0,0 +1,22 @@
+# l = ["gfg", "is", "piyush", "best"]
+
+# def myFun(s):
+#     return len(s)
+
+# # l.sort(key=myFun)
+# l.sort(key=len, reverse=True)
+# print(l)
+
+# class Point:
+#     def __init__(self, x, y):
+#         self.x = x
+#         self.y = y
+
+# def myFun(p):
+#     return p.x
+
+# l = [Point(1, 15), Point(10, 5), Point(3, 8)]
+# l.sort(key=myFun)
+
+# for i in l:
+#     print(i.x, i.y)
