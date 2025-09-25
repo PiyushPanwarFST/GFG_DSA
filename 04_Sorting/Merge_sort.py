908f1f2b9a9986dbc04c156f5f7376b00040fa00
PiyushPanwarFST <piyush2002panwar@gmail.com>
2025-09-25 19:19:11 +0530

    modification

--- 04_Sorting/Merge_sort.py
+++ 04_Sorting/Merge_sort.py
@@ -1,18 +1,42 @@
-a = [10, 15, 20]
-b = [5, 6, 6, 30, 40]
+def merge(a, low, mid, high):
+    left = a[low:mid + 1]
+    right = a[mid + 1:high + 1]
 
-def merge(a, b):
-    i = 0
-    j = 0
-    c = []
+    i = j = 0
+    k = low
 
-    while i < len(a) and j < len(b):
-        if a[i] < b[j]:
-            c.append(a[i])
+    while i < len(left) and j < len(right):
+
+        if left[i] <= right[j]:
+            a[k] = left[i]
+
+            k += 1
             i += 1
-        elif a[i] > b[j]:
-            c.append(b[j])
+        else:
+            a[k] = right[j]
+            k += 1
             j += 1
-    return c + a[i:] + b[j:]
 
-print(merge(a, b))
+    while i < len(left):
+        a[k] = left[i]
+        i += 1
+        k += 1
+
+    while j < len(right):
+        a[k] = right[j]
+        j += 1
+        k += 1
+
+
+def mergeSort(arr, l, r):
+    if r > l:
+        m = (r + l) // 2
+        mergeSort(arr, l, m)
+        mergeSort(arr, m + 1, r)
+        merge(arr, l, m, r)
+
+
+arr = [10, 5, 30, 15, 7]
+
+mergeSort(arr, 0, 4)
+print(*arr)
